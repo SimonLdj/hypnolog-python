c0a61a6dda3407a900bb14a60702f5d125d2aedc
Simon Ldj <simon.ldj@gmail.com>
2018-03-31 03:25:00 +0300

    Add in code documentation

--- hypnolog.py
+++ hypnolog.py
@@ -9,8 +9,16 @@ _errorHandler = None;
 _host = 'localhost';
 _port = 7000;
 
-# Init
 def initialize(host=None, port=None, errorHandler=None):
+    '''
+    Initialize logger and set configuration
+
+
+    :param string   host:           HypnoLog server host name. Default is "localhost"
+    :param int      port:           HypnoLog server port number. Default is 7000
+    :param function errorHandler:   Function to handle when inner HypnoLog error occure. Of
+                                    signature (error). If not given, default behavior will be used.
+    '''
     global _host, _port, _errorHandler;
     if host != None:
         _host = host;
@@ -19,8 +27,14 @@ def initialize(host=None, port=None, errorHandler=None):
     if errorHandler != None:
         _errorHandler = errorHandler;
 
-# Simple HypnoLog logging funciton
 def log(obj, objType=None):
+    '''
+    Log given object using HypnoLog.
+
+    :param object obj:      The object to be logged
+    :param string objType:  String represent the type of the logged object, will determine how the
+                            object is visualized. If not provided, `object` type will be used
+    '''
     try:
         if objType == None:
             objType = _determineType(obj);
